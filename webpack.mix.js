50c24e3aa44a14a77fca05c3fe91d5c189a79bfb
yoimaral <yalozano9@misena.edu.co>
2020-06-21 14:29:53 -0500

    hasta aqui funciona el restablecimiento de correo

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const mix = require("laravel-mix");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/main.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .sass("resources/sass/main.scss", "public/css");
+
+mix.browserSync("http://proyectox.test");
+if (mix.inProduction()) {
+    mix.version();
+}
